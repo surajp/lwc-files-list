cc34df07a5fa109e69cb7eadc20df77215bcbe50
Suraj Pillai <85.suraj@gmail.com>
2020-04-10 00:47:01 -0400

    WIP: First commit

---
+++ force-app/main/default/lwc/filesRelatedList/filesRelatedList.js
@@ -0,0 +1,58 @@
+import { LightningElement, wire, api } from "lwc";
+import getRelatedFiles from "@salesforce/apex/GetFilesController.getFilesList";
+import getFileVersionDetails from "@salesforce/apex/GetFilesController.getFileVersionDetails";
+
+const actions = [{ label: "Version Details", name: "show_details" }];
+
+const columns = [
+  { label: "Title", fieldName: "title" },
+  { label: "Id", fieldName: "url", type: "url" },
+  { label: "Uploaded Date", fieldName: "createdDate", type: "date" },
+  { label: "Uploaded by", fieldName: "createdBy", type: "string" },
+  { type: "action", typeAttributes: { rowActions: actions } }
+];
+
+const versionColumns = [
+  { label: "Title", fieldName: "title" },
+  { label: "Download Link", fieldName: "url", type: "url" },
+  { label: "Uploaded Date", fieldName: "createdDate", type: "date" },
+  { label: "Uploaded by", fieldName: "createdBy", type: "string" }
+];
+
+export default class FilesRelatedList extends LightningElement {
+  @api
+  recordId;
+
+  files = [];
+  showModal = false;
+  columns = columns;
+  versionColumns = versionColumns;
+  versionDetails = [];
+
+  @wire(getRelatedFiles, { recordId: "$recordId" })
+  getFilesList({ error, data }) {
+    if (!error && data) {
+      this.files = data;
+      console.log("files found " + JSON.stringify(this.files));
+    }
+  }
+
+  handleRowAction(event) {
+    console.log(">> handle row action " + event.detail.action.name);
+    const row = event.detail.row;
+    this.showVersionDetails(row.id);
+  }
+
+  showVersionDetails(id) {
+    console.log(">> file version details");
+    getFileVersionDetails({ fileId: id })
+      .then((result) => {
+        console.log(">> version details " + JSON.stringify(result));
+        this.versionDetails = result;
+        this.showModal = true;
+      })
+      .catch((err) => {
+        console.error(JSON.stringify(err));
+      });
+  }
+}
