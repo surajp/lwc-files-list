2eff2c075f724eb8d2d8e1ce247d1322b3b08835
Suraj Pillai <85.suraj@gmail.com>
2020-04-22 00:20:50 -0400

    Add file upload

--- force-app/main/default/lwc/filesRelatedList/filesRelatedList.js
+++ force-app/main/default/lwc/filesRelatedList/filesRelatedList.js
@@ -1,8 +1,13 @@
 import { LightningElement, wire, api } from "lwc";
 import getRelatedFiles from "@salesforce/apex/GetFilesController.getFilesList";
 import getFileVersionDetails from "@salesforce/apex/GetFilesController.getFileVersionDetails";
+import { createRecord } from "lightning/uiRecordApi";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-const actions = [{ label: "Version Details", name: "show_details" }];
+const actions = [
+  { label: "Version Details", name: "show_details" },
+  { label: "Upload", name: "upload_version" }
+];
 
 const columns = [
   {
@@ -23,6 +28,7 @@ const versionColumns = [
     type: "url",
     typeAttributes: { label: "Download" }
   },
+  { label: "Version Reason", fieldName: "versionReason", type: "string" },
   { label: "Uploaded Date", fieldName: "createdDate", type: "date" },
   { label: "Uploaded by", fieldName: "createdBy", type: "string" }
 ];
@@ -36,6 +42,8 @@ export default class FilesRelatedList extends LightningElement {
   columns = columns;
   versionColumns = versionColumns;
   versionDetails = [];
+  fileUpload = false;
+  _currentDocId = null;
 
   @wire(getRelatedFiles, { recordId: "$recordId" })
   getFilesList({ error, data }) {
@@ -47,18 +55,32 @@ export default class FilesRelatedList extends LightningElement {
 
   closeModal() {
     this.showModal = false;
+    this._currentDocId = null;
+    this.fileUpload = false;
     this.versionDetails = [];
   }
 
   handleRowAction(event) {
     console.log(">> handle row action " + event.detail.action.name);
-    const row = event.detail.row;
-    this.showVersionDetails(row.id);
+    const action = event.detail.action.name;
+    this._currentDocId = row.id;
+    if (action == "show_details") {
+      const row = event.detail.row;
+      this.fileUpload = false;
+      this.showVersionDetails();
+    } else if (action == "upload_version") {
+      this.fileUpload = true;
+    }
   }
 
-  showVersionDetails(id) {
+  newFileUpload() {
+    this.showModal = true;
+    this.fileUpload = true;
+  }
+
+  showVersionDetails() {
     console.log(">> file version details");
-    getFileVersionDetails({ fileId: id })
+    getFileVersionDetails({ fileId: this._currentDocId })
       .then((result) => {
         console.log(">> version details " + JSON.stringify(result));
         this.versionDetails = result;
@@ -67,5 +89,37 @@ export default class FilesRelatedList extends LightningElement {
       .catch((err) => {
         console.error(JSON.stringify(err));
       });
+  }
+
+  handleUpload(event) {
+    event.preventDefault();
+    const file = this.template.querySelector("input.file").files[0];
+    const reader = new FileReader();
+    let fileData = "";
+    reader.onload = () => {
+      fileData = reader.result;
+      this._uploadFile(file, fileData);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  _uploadFile(file, fileData) {
+    const payload = {
+      Title: file.name,
+      PathOnClient: file.name,
+      VersionData: fileData.replace(BASE64EXP, "")
+    };
+    if (this._currentDocId) {
+      payload.ContentDocumentId = this._currentDocId;
+    }
+    createRecord(payload).then(() => {
+      this.closeModal();
+      this.dispatchEvent(
+        new ShowToastEvent({
+          variant: "success",
+          message: `Content Document Link created`
+        })
+      );
+    });
   }
 }
